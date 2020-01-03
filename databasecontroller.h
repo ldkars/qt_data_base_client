36de0b8931bb448c29866a9ce8c0a4de634c6fb3
ldkars <igra.22play@bk.ru>
2020-01-04 00:22:59 +0300

    Create controller and add connect to bd

--- databasecontroller.h
+++ databasecontroller.h
@@ -1,11 +1,15 @@
 #ifndef DATABASECONTROLLER_H
 #define DATABASECONTROLLER_H
+#include <QString>
+#include <QtSql>
 
 
 class DataBaseController
 {
 public:
     DataBaseController();
+    QSqlDatabase db;
+    bool connect(QString login, QString password);
 };
 
 #endif // DATABASECONTROLLER_H
