8f9b5808b092c3d0588e8df912792d2ccca42560
ldkars <igra.22play@bk.ru>
2020-01-04 01:14:35 +0300

    Create controller. add connect func

--- dbcontrollee.cpp
+++ dbcontrollee.cpp
@@ -4,3 +4,24 @@ dbcontrollee::dbcontrollee()
 {
 
 }
+
+bool dbcontrollee::connect(QString login, QString password){
+    db = QSqlDatabase();
+    db.removeDatabase("example-connection"); // remove old connection if exists
+    db = QSqlDatabase::addDatabase("QODBC", "example-connection");
+
+    QString driver = "SQL Server";
+    QString server = "DESKTOP-F4HC3G1\\SQLEXPRESS";
+    int port = 1433;
+    QString database  = "test";
+    const QString connection_string_sqlauth =
+            QString("DRIVER={%1};SERVER=%2;PORT=%3;DATABASE=%4;UID=%5;PWD=%6");
+
+    qDebug() << connection_string_sqlauth.arg(driver).arg(server).arg(port).arg(database)
+                .arg(login).arg(password);
+
+    db.setDatabaseName(connection_string_sqlauth.arg(driver).arg(server).arg(port).arg(database)
+                       .arg(login).arg(password));
+
+    return db.open();
+}
--- dbcontrollee.h
+++ dbcontrollee.h
@@ -1,11 +1,15 @@
 #ifndef DBCONTROLLEE_H
 #define DBCONTROLLEE_H
-
+#include <QString>
+#include <QtSql>
+#include <QDebug>
 
 class dbcontrollee
 {
 public:
     dbcontrollee();
+    QSqlDatabase db;
+    bool connect(QString login, QString password);
 };
 
 #endif // DBCONTROLLEE_H
